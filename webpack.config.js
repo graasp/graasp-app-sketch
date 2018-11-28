82e7d61a60f0ae3106fa9f0d14cfd1ee96b8ae5e
Juan Carlos Farah <juancarlos.farah@epfl.ch>
2018-11-28 17:04:33 +0100

    fix: fix one-file minification
    closes #11

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,33 @@ const path = require('path');
 const glob = require('glob');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const InterpolateHtmlPlugin = require('interpolate-html-plugin');
 const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
 // const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
 
+// from: https://github.com/facebook/create-react-app/blob/master/packages/react-scripts/config/env.js
+// grab react app environment variables
+const REACT_APP = /^REACT_APP_/i;
+const REACT_ENV = Object
+  .keys(process.env)
+  .filter(key => REACT_APP.test(key))
+  .reduce((env, key) => {
+    // eslint-disable-next-line no-param-reassign
+    env[key] = process.env[key];
+    return env;
+  },
+  {
+    // minified version should always be in production
+    NODE_ENV: 'production',
+    // minified version should always relative base
+    PUBLIC_URL: '.',
+  });
 
 module.exports = {
   entry: {
-    'bundle.js': glob.sync('build/static/?(js|css)/main.*.?(js|css)').map(f => path.resolve(__dirname, f)),
+    'bundle.js': glob
+      .sync('build/static/?(js|css)/*.?(js|css)')
+      .map(f => path.resolve(__dirname, f)),
   },
   output: {
     filename: 'dist/bundle.min.js',
@@ -25,7 +45,7 @@ module.exports = {
   plugins: [
     new UglifyJsPlugin(),
     new HtmlWebpackPlugin({
-      filename: 'dist/index.html',
+      filename: 'index.html',
       template: 'public/index.html',
       inlineSource: '.(js|css)$',
     }),
@@ -33,5 +53,6 @@ module.exports = {
       inline: 'bundle',
       preload: /\.js$/,
     }),
+    new InterpolateHtmlPlugin(REACT_ENV),
   ],
 };
